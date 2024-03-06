64143e4966f03ec2bfee47e5e5486ebbe6fd06f0
Navin Tiwari <navintiwari08@yahoo.com>
2024-03-06 21:43:57 +0530

    Add update_snapshot() to keep track of live tiles.

--- game-of-life/gol.py
+++ game-of-life/gol.py
@@ -1,6 +1,7 @@
 import pyglet
 from settings import *
-import random
+from pyglet.window import mouse
+from pyglet.window import key
 
 class BlackTile(pyglet.sprite.Sprite):
     def __init__(self, **kwargs):
@@ -24,15 +25,18 @@ class GameOfLife(pyglet.window.Window):
         rows, cols = self.height//SPRITE_HEIGHT, self.width//SPRITE_WIDTH
         self.snapshot = [[0 for _ in range(cols)] for _ in range(rows)]
         self.sprites = {}
+        self.update_q = {}
+        self.alive = set()
         for r in range(rows):
             for c in range(cols):
                 x, y = c*SPRITE_WIDTH, r*SPRITE_HEIGHT
                 self.sprites[(r,c)] = BlueTile(x=x, y=y, batch=self.batch)
     
     def on_mouse_press(self, x, y, button, modifiers):
-        c = x // SPRITE_WIDTH
-        r = y // SPRITE_HEIGHT
-        self.toggle_tile(r,c)
+        if (button & mouse.LEFT):
+            c = x // SPRITE_WIDTH
+            r = y // SPRITE_HEIGHT
+            self.toggle_tile(r,c)
 
     def on_draw(self):
         self.clear()
@@ -41,10 +45,23 @@ class GameOfLife(pyglet.window.Window):
     def toggle_tile(self, r, c):
         self.sprites[(r,c)].life = 1 - self.sprites[(r,c)].life
         self.sprites[(r,c)].image = self.imgs[self.sprites[(r,c)].life]
+        if (r,c) in self.update_q:
+            del self.update_q[(r,c)]
+        else:
+            self.update_q[(r,c)] = self.sprites[(r,c)].life
 
     def update(self, dt):
-        pass
+        self.update_snapshot()
+        print("something")
 
+    def update_snapshot(self):
+        for (r,c), life in self.update_q.items():
+            if self.sprites[(r,c)].life:
+                self.alive.add((r,c))
+            elif (r,c) in self.alive:
+                self.alive.remove((r,c))
+            self.snapshot[r][c] = life
+        self.update_q.clear()
 
 if __name__ == "__main__":
     pyglet.resource.path = [SPRITES_DIR]
@@ -52,6 +69,6 @@ if __name__ == "__main__":
     width, height = WIN_SIZE
     width = (width // SPRITE_WIDTH) * SPRITE_WIDTH
     height = (height // SPRITE_HEIGHT) * SPRITE_HEIGHT
-    gol = GameOfLife(width, height, caption="Game of Life")
-    pyglet.clock.schedule_interval(gol.update, interval=0.2)
+    gol = GameOfLife(width, height, caption="Game of Life: select live tiles and hit spacebar!")
+    pyglet.clock.schedule_interval(gol.update, interval=5.0)
     pyglet.app.run()
