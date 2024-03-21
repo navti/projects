fb89ee844b11aa4a09345f28517ef7de09ae67e2
Navin Tiwari <navintiwari08@yahoo.com>
2024-03-21 22:09:32 +0530

    Use glfw for windowing functions. Add skeleton App class.

--- cube/main.py
+++ cube/main.py
@@ -1,20 +1,33 @@
+import glfw
+from glfw.GLFW import *
 from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
+from settings import *
 
 
-def display():
-    glClear(GL_COLOR_BUFFER_BIT)
-    glFlush()
+class App:
+    def __init__(self):
+        glfw.init()
+        glfw.window_hint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR)
+        glfw.window_hint(GLFW_CONTEXT_VERSION_MINOR, GL_MINOR)
+        glfw.window_hint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+        glfw.window_hint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE)
+        self.window = glfw.create_window(WINDOW_WIDTH, WINDOW_HEIGHT, "The Cube", None, None)
+        glfw.make_context_current(self.window)
+        glClearColor(1,1,1,1)
 
-def main():
-    glutInit()
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
-    glutInitWindowSize(500, 500)
-    glutCreateWindow("Simple window!")
-    glClearColor(1,1,1,1)
-    glutDisplayFunc(display)
-    glutMainLoop()
+    def run(self):
+        while not glfw.window_should_close(self.window):
+            if glfw.get_key(self.window, GLFW_KEY_ESCAPE) == GLFW_PRESS:
+                break
+            glfw.poll_events()
+            glClear(GL_COLOR_BUFFER_BIT)
+            glfw.swap_buffers(self.window)
 
-if __name__ == '__main__':
-    main()
+    def terminate(self):
+        glfw.destroy_window(self.window)
+        glfw.terminate()
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
+    app.terminate()
